31f131684e4f1f1fd81e1aa60f3faaf9c36769a7
Sébastien Journiac <sebastien.journiac@le-campus-numerique.fr>
2024-03-04 09:06:28 +0100

    Add payment_methods_table.php

--- app/Models/paymentMethod.php
+++ app/Models/paymentMethod.php
@@ -5,7 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class books extends Model
+class paymentMethod extends Model
 {
     use HasFactory;
+
+
 }
