cd1fd25c26dd29b53e59feefd87f2dbe33d6b8f1
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-04 14:12:28 +0100

    table d'association

---
+++ database/migrations/2024_03_04_130720_create_books_has_images_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books_has_images', function (Blueprint $table) {
+            $table->uuidid();
+            $table->foreignUuid('books_id')->constrained();
+            $table->foreignUuid('images_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books_has_images');
+    }
+};
