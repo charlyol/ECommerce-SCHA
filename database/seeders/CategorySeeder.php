b6ba90d66394b16e7797393417de089593cd57bd
AugustinPech <80385451+AugustinPech@users.noreply.github.com>
2024-03-06 09:39:57 +0100

    Models : foreign ardresses-user

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -13,7 +13,7 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         Category::factory()
-            ->count(5)
+            ->count(2)
             ->create();
     }
 }
