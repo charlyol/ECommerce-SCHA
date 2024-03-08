ba598a4df1fa4cacc12b8d0c3ff8215bdb3f4893
Sébastien Journiac <sebastien.journiac@le-campus-numerique.fr>
2024-03-08 13:39:26 +0100

    On a fait des bétises ?

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,9 +1,4 @@
 @props(['value'])
-
-<<<<<<< HEAD
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
-=======
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
->>>>>>> develop
     {{ $value ?? $slot }}
 </label>
