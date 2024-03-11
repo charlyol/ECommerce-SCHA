d998ba5eea0a5dfc77223ff50ff097b3ed956a78
charlyol <147928338+charlyol@users.noreply.github.com>
2024-03-11 15:58:36 +0100

    just for fun

--- resources/views/catalog/index.blade.php
+++ resources/views/catalog/index.blade.php
@@ -1,4 +1,5 @@
-<!DOCTYPE html>
+@extends('layouts.navigation')
+    <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
@@ -15,12 +16,16 @@
 @if (Route::has('login'))
     <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
         @auth
-            <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
+            <a href="{{ url('/dashboard') }}"
+               class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
         @else
-            <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
+            <a href="{{ route('login') }}"
+               class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
+                in</a>
 
             @if (Route::has('register'))
-                <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                <a href="{{ route('register') }}"
+                   class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
             @endif
         @endauth
     </div>
@@ -29,11 +34,11 @@
 <div class="grid grid-cols-3 gap-4">
     @foreach($catalog as $product)
         <div class="card w-96 bg-base-100 shadow-xl">
-        <div class="card-body bg-gray-200 p-4">
-            <h2 class="card-title">{{$product->title}}</h2>
-            <p class="card-actions justify-end">{{$product->description}}</p>
-            <p class="btn btn-primary">Prix : {{$product->price_wt}}€</p>
-        </div>
+            <div class="card-body bg-gray-200 p-4">
+                <h2 class="card-title">{{$product->title}}</h2>
+                <p class="card-actions justify-end">{{$product->description}}</p>
+                <p class="btn btn-primary">Prix : {{$product->price_wt}}€</p>
+            </div>
         </div>
     @endforeach
 </div>
